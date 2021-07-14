4fedbdc04e92f70db808460d275d3586bd8ce487
Detlef Dumpelmann <Dumpelmann.detlef@gmail.com>
2021-07-14 16:41:23 +0200

    index.html with tests adapted

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "src")));
 
 app.get("/", (req, res) => {
   res.sendStatus(200);
+  res.sendFile(__dirname + "/src/content/index.html");
+});
+app.get("/data", (req, res) => {
+  res.send({});
 });
 
 export default app;
